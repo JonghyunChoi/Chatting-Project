ae6952192b3cde0b77805cd0a32a159a1c5d346b
JonghyunChoi <chlwhdgus86@naver.com>
2021-10-12 22:45:22 +0900

    first commit

---
+++ src/test/java/kr/ac/uc/webchatting/WebChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.uc.webchatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
