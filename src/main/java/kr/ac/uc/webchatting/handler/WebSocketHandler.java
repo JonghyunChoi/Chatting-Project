0633bf4bd929aa1d7456f9de869497ac5d02cee1
Uz <a1zefd6@gmail.com>
2021-11-03 16:29:31 +0900

    gradle 업데이트
    websocket config/handler 추가

---
+++ src/main/java/kr/ac/uc/webchatting/handler/WebSocketHandler.java
@@ -0,0 +1,23 @@
+package kr.ac.uc.webchatting.handler;
+
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+
+        TextMessage textMessage = new TextMessage("Welcome chat sever");
+        session.sendMessage(textMessage);
+    }
+}
