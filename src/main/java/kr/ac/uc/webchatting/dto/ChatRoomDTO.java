f8fcf5ae49397d9ab41cd3e44024d4c0b9ddc761
Uz <a1zefd6@gmail.com>
2021-11-10 02:10:05 +0900

    채팅 메시지를 주고받기 위한 DTO (입장, 대화)
    채팅방에 세션 Session 추가 (입장 메시지 추가)
    
    채팅 서비스 구현

--- src/main/java/kr/ac/uc/webchatting/dto/ChatRoomDTO.java
+++ src/main/java/kr/ac/uc/webchatting/dto/ChatRoomDTO.java
@@ -1,6 +1,11 @@
 package kr.ac.uc.webchatting.dto;
 
+import kr.ac.uc.webchatting.service.ChatService;
 import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class ChatRoomDTO {
@@ -8,4 +13,18 @@ public class ChatRoomDTO {
     private String room_name;
     private String master_id;
     private int public_open;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+
+    public void handleActions(WebSocketSession session, ChatDTO chatMessage, ChatService chatService) {
+        if (chatMessage.getType().equals(ChatDTO.MessageType.ENTER)) {
+            sessions.add(session);
+            chatMessage.setMessage(chatMessage.getSender() + "님이 입장 했습니다.");
+        }
+        sendMessage(chatMessage, chatService);
+    }
+
+    public <T> void sendMessage(T message, ChatService chatService) {
+        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+    }
 }
+
--- src/main/java/kr/ac/uc/webchatting/handler/WebSocketHandler.java
+++ src/main/java/kr/ac/uc/webchatting/handler/WebSocketHandler.java
@@ -1,15 +1,23 @@
 package kr.ac.uc.webchatting.handler;
 
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.ac.uc.webchatting.dto.ChatDTO;
+import kr.ac.uc.webchatting.dto.ChatRoomDTO;
+import kr.ac.uc.webchatting.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -17,7 +25,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
         String payload = message.getPayload();
         log.info("payload {}", payload);
 
-        TextMessage textMessage = new TextMessage("Welcome chat sever");
-        session.sendMessage(textMessage);
+        ChatDTO chatMessage = objectMapper.readValue(payload, ChatDTO.class);
+        ChatRoomDTO room = chatService.findRoomById(chatMessage.getRoom_id());
+        room.handleActions(session, chatMessage, chatService);
     }
 }
