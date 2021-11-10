e58e4efaeb890d6dc5426e23d73abc1a8eb3e009
Uz <a1zefd6@gmail.com>
2021-11-10 15:07:05 +0900

    gradle update
    STOMP update

--- src/main/java/kr/ac/uc/webchatting/dto/ChatRoomDTO.java
+++ src/main/java/kr/ac/uc/webchatting/dto/ChatRoomDTO.java
@@ -1,6 +1,5 @@
 package kr.ac.uc.webchatting.dto;
 
-import kr.ac.uc.webchatting.service.ChatService;
 import lombok.Data;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -13,18 +12,7 @@ public class ChatRoomDTO {
     private String room_name;
     private String master_id;
     private int public_open;
+
     private Set<WebSocketSession> sessions = new HashSet<>();
-
-    public void handleActions(WebSocketSession session, ChatDTO chatMessage, ChatService chatService) {
-        if (chatMessage.getType().equals(ChatDTO.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장 했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
-
-    public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
-    }
 }
 
