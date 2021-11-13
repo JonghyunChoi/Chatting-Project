d72ea564feecb6cb48bb3e8f5f0d1806a2933483
Jonghyun <chlwhdgus86@naver.com>
2021-11-13 18:08:51 +0900

    modify design

--- src/main/java/kr/ac/uc/webchatting/dao/IChatRoomDAO.java
+++ src/main/java/kr/ac/uc/webchatting/dao/IChatRoomDAO.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 @Mapper
 public interface IChatRoomDAO {
-    public void insertChatRoom(ChatRoomDTO dto);
-    public List<ChatRoomDTO> myRoomList(String id);
-    public List<ChatRoomDTO> publicRoomList();
-    public int getChatRoomID();
+    public void insertChatRoom(ChatRoomDTO dto);    // 채팅방 생성
+    public List<ChatRoomDTO> myRoomList(String id); // 자신이 참여하고 있는 채팅방 불러오기
+    public List<ChatRoomDTO> publicRoomList();      // 공개 채팅방 불러오기
+    public int getChatRoomID();                     // 최신 채팅방 번호 가져오기
 }
