08f1c03361885770e7b0e4a86f080ba78486a61a
Uz <a1zefd6@gmail.com>
2021-11-17 17:17:00 +0900

    Websocket delete

--- src/main/java/kr/ac/uc/webchatting/dto/ChatRoomDTO.java
+++ src/main/java/kr/ac/uc/webchatting/dto/ChatRoomDTO.java
@@ -1,10 +1,6 @@
 package kr.ac.uc.webchatting.dto;
 
 import lombok.Data;
-import org.springframework.web.socket.WebSocketSession;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 public class ChatRoomDTO {
@@ -15,6 +11,5 @@ public class ChatRoomDTO {
     private int total_people;
     private int public_open;
 
-    private Set<WebSocketSession> sessions = new HashSet<>();
 }
 
