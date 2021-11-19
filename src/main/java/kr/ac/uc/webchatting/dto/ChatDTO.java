3930901c9e3f8567dd7a59c28d71efe2c08ce0bd
Uz <a1zefd6@gmail.com>
2021-11-20 06:30:43 +0900

    ChatDAO/DTO add

---
+++ src/main/java/kr/ac/uc/webchatting/dto/ChatDTO.java
@@ -0,0 +1,14 @@
+package kr.ac.uc.webchatting.dto;
+
+import lombok.Data;
+
+@Data
+public class ChatDTO {
+
+    int chatID;
+    String fromID;
+    String toID;
+    String chatContent;
+    String chatTime;
+
+}
